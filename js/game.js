55ee2274c3435747d89e788630712ee41abfc91b
Fábio Gouveia Silva <gouveia.fabio98@gmail.com>
2025-02-25 21:55:47 +0000

    +

--- js/game.js
+++ js/game.js
@@ -428,19 +428,3 @@ function playMusic() {
 function isMobileDevice() {
     return /Mobi|Android/i.test(navigator.userAgent);
 }
-
-function saveHighscore(score, difficulty) {
-    let highscores = getHighscores();
-    highscores.push({ score: score, difficulty: difficulty });
-    highscores.sort((a, b) => b.score - a.score);
-    if (highscores.length > 10) {
-        highscores = highscores.slice(0, 10);
-    }
-    localStorage.setItem("highscores", JSON.stringify(highscores));
-}
-
-function getHighscores() {
-    let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-    highscores.sort((a, b) => b.score - a.score);
-    return highscores;
-}
